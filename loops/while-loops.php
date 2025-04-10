893399cb3ab0cf9c482f18f41946df5fc4435e50
Cameron Wheatley <113945049+cameronjamesw@users.noreply.github.com>
2025-04-10 19:04:50 +0000

    complete while loops lesson

---
+++ loops/while-loops.php
@@ -0,0 +1,33 @@
+<?php
+
+$a = 0;
+// While loop
+do {
+  echo "Hello everyone!! \n";
+  $a++;
+} while ($a < 10);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Do-While Loops</title>
+</head>
+
+<body>
+  <div>
+    <?php
+    $a = 0;
+
+    while ($a < 15) {
+      echo "<p> Hello everyone!! This is the $a increment! </p>";
+      $a++;
+    }
+    ?>
+  </div>
+</body>
+
+</html>
