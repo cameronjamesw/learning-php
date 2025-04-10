c28da14b799423a1682f407f3680b89d3992993a
Cameron Wheatley <113945049+cameronjamesw@users.noreply.github.com>
2025-04-10 12:07:38 +0000

    Complete concatentation lesson

---
+++ concat.php
@@ -0,0 +1,13 @@
+<?php
+$greeting = "Hello there";
+
+$first_name = "Cameron";
+$last_name = "Wheatley";
+
+$full_name = $first_name . " " . $last_name;
+
+$fiance_name = "Hannah";
+
+echo "<h1>$greeting" . " " . "$full_name</h1>";
+
+echo "<h1>$greeting" . " " . $fiance_name . " " . "$last_name</h1>";
